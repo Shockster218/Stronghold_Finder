8f01540d65dea0e9683057b19c4b5f827cc03aa9
shockster218 <shockster218@gmail.com>
2020-10-31 13:33:00 -0400

    added curses window with exiting and resetting. Next just to parse coordinates clipboard then finished

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import numpy as np
+import pyperclip
+import msvcrt
+import curses
 
-#hitCircle calculates where the first stronghold generation ring starts, where the player would "hit" it, moving directly forward
-#and calculates the position to the second ender eye throw
-def hitCircle(pX,pZ,angle):
+# hitCircle calculates where the first stronghold generation ring starts, where the player would "hit" it, moving directly forward
+# and calculates the position to the second ender eye throw
+
+
+def hitCircle(pX, pZ, angle):
     xHit = None
     yHit = None
     cos = np.cos(angle*np.pi/180)
-    #if the stronghold is at the +X
+    # if the stronghold is at the +X
     if cos >= 0:
-        x = np.linspace(pX-10, 2700,2700)
+        x = np.linspace(pX-10, 2700, 2700)
         a = np.tan(angle*np.pi/180)
         b = pZ - pX * a
         y = a*x + b
@@ -17,8 +22,8 @@ def hitCircle(pX,pZ,angle):
                 xHit = x[i]
                 yHit = y[i]
                 break
-        pos1 = (xHit,yHit)
-        x2 = np.linspace(xHit, xHit+100,500)
+        pos1 = (xHit, yHit)
+        x2 = np.linspace(xHit, xHit+100, 500)
         a2 = -1/np.tan(angle*np.pi/180)
         b2 = yHit - xHit * a2
         y2 = a2*x2 + b2
@@ -26,10 +31,10 @@ def hitCircle(pX,pZ,angle):
             if abs(x2[i] - xHit)**2 + abs(y2[i] - yHit)**2 >= 42*42:
                 xST = x2[i]
                 yST = y2[i]
-        pos2 = (xST,yST)
-    #if the stronghold is at the -X
+        pos2 = (xST, yST)
+    # if the stronghold is at the -X
     else:
-        x = np.linspace(pX+10, -2700,2700)
+        x = np.linspace(pX+10, -2700, 2700)
         a = np.tan(angle*np.pi/180)
         b = pZ - pX * a
         y = a*x + b
@@ -38,8 +43,8 @@ def hitCircle(pX,pZ,angle):
                 xHit = x[i]
                 yHit = y[i]
                 break
-        pos1 = (xHit,yHit)
-        x2 = np.linspace(xHit, xHit+100,500)
+        pos1 = (xHit, yHit)
+        x2 = np.linspace(xHit, xHit+100, 500)
         a2 = -1/np.tan(angle*np.pi/180)
         b2 = yHit - xHit * a2
         y2 = a2*x2 + b2
@@ -47,57 +52,129 @@ def hitCircle(pX,pZ,angle):
             if abs(x2[i] - xHit)**2 + abs(y2[i] - yHit)**2 >= 42*42:
                 xST = x2[i]
                 yST = y2[i]
-        pos2 = (xST,yST)
-        
-    return (pos1,pos2)
-    
+        pos2 = (xST, yST)
+
+    return (pos1, pos2)
+
+
 def StrongholdCoords():
-    #stabilishing the variables
+    # stabilishing the variables
     f3c0 = input()
     f3c0 = f3c0[42:]
     f3c0 = f3c0.split()
-    px0 = float(f3c0[0]) 
+    px0 = float(f3c0[0])
     pz0 = float(f3c0[2])
-    angle0 = float(f3c0[3])%360
+    angle0 = float(f3c0[3]) % 360
 
-    #translating minecraft angles to daily life cartesian angles
+    # translating minecraft angles to daily life cartesian angles
     if angle0 >= 0:
-        angle0 = (angle0+90)%360
+        angle0 = (angle0+90) % 360
     else:
-        angle0 = (angle0-270)%360
-    
-    #distance from origin
+        angle0 = (angle0-270) % 360
+
+    # distance from origin
     distOrigin = np.sqrt(px0*px0 + pz0*pz0)
-    #print("You're this far from the Origin: ", distOrigin)
-    
+    # print("You're this far from the Origin: ", distOrigin)
+
     if distOrigin >= 1400:
         print("Move 27 blocks perpendicularly to the Ender Eye flight direction and throw the second one. (27 blocks = 4 seconds sprinting)")
-    
+
     else:
-        circlePoint, secThrowPoint = hitCircle(px0,pz0,angle0)
+        circlePoint, secThrowPoint = hitCircle(px0, pz0, angle0)
         print("Go to: ", secThrowPoint, "\nCircle starts at: ", circlePoint)
-    
-    #stabilishing the variables
+
+    # stabilishing the variables
     f3c1 = input()
     f3c1 = f3c1[42:]
     f3c1 = f3c1.split()
     px1 = float(f3c1[0])
     pz1 = float(f3c1[2])
-    angle1 = float(f3c1[3])%360
-        
-    #translating minecraft angles to daily life cartesian angles    
+    angle1 = float(f3c1[3]) % 360
+
+    # translating minecraft angles to daily life cartesian angles
     if angle1 >= 0:
-        angle1 = (angle1+90)%360
+        angle1 = (angle1+90) % 360
     else:
-        angle1 = (angle1-270)%360
-    
-    #calculating stronghold position
+        angle1 = (angle1-270) % 360
+
+    # calculating stronghold position
     a0 = np.tan(angle0*np.pi/180)
     a1 = np.tan(angle1*np.pi/180)
     b0 = pz0 - px0 * a0
     b1 = pz1 - px1 * a1
     pxS = (b1 - b0)/(a0 - a1)
     pzS = pxS * a0 + b0
-    
-    #printing
+
+    # printing
     print("Stronghold is at: ", (pxS, pzS), " GOOD LUCK :D")
+
+
+stdscr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+stdscr.nodelay(1)
+stdscr.keypad(True)
+exit = False
+newrun = False
+uInput = ""
+
+
+def initWindow():
+    stdscr.addstr(0, 10, "Stronghold finder by Brandon Giannos aka Shockster_")
+    stdscr.addstr(2, 0, "This runs nether coords: ")
+    stdscr.addstr(4, 0, "Suggested 2nd throw coords: ")
+    stdscr.addstr(5, 0, "Stronghold location: ")
+    stdscr.move(7, 0)
+
+
+def parseClipboard(clip):
+    pass
+
+
+def addNetherCoords(x, y):
+    pass
+
+
+def addSecondThrow(x, y):
+    pass
+
+
+def addStrongholdCoords(x, y):
+    pass
+
+
+
+def addUserInput(inp):
+    global uInput
+    global exit
+    if inp == 10:
+        if uInput == "exit":
+            stdscr.keypad(False)
+            curses.echo()
+            curses.endwin()
+            stdscr.clear()
+            exit = True
+    elif inp == 8:
+        uInput = uInput[:-1]
+        stdscr.delch(7, len(uInput))
+    elif chr(inp).isalpha():
+        uInput = uInput + chr(inp)
+        stdscr.addstr(7, 0, uInput)
+
+initWindow()
+
+while not exit:
+    char = stdscr.getch()
+    if char != -1:
+        #reset key tilde
+        if char == 96:
+            initWindow()
+        else:
+            addUserInput(char)
+    else:
+        try:
+            clipboard = pyperclip.waitForNewPaste(1)
+            parseClipboard(clipboard)
+        except:
+            pass:
+    stdscr.refresh()
